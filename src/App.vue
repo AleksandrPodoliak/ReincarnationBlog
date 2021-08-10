5a772d71a66dcac522838f61926c6068d682cf33
Aleksandr Podoliak <aleksandr.podoljak@gmail.com>
2021-08-10 23:51:36 +0300

    section1

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,26 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+    <Header></Header>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from "/src/components/Header.vue";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  components: { Header },
+};
 </script>
 
 <style lang="scss">
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  width: 1920px;
+  margin: 0 auto;
 }
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,17 @@
+<template>
+  <header></header>
+</template>
+
+<script>
+export default {
+  name: "Header",
+};
+</script>
+
+<style scoped lang="scss">
+header {
+  width: 100%;
+  height: 889px;
+  background-image: url("/src/assets/header_back.png");
+}
+</style>
